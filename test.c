edb1019b62ac07c279248b4e22a7419e92be9249
yukar1z0e <44317939+yukar1z0e@users.noreply.github.com>
2019-09-30 10:44:03 +0800

    note

---
+++ test.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#define PASSWORD "1234567"
+int verify_password(char *password)
+{
+    int authenticated;
+    authenticated = strcmp(password, PASSWORD);
+    return authenticated;
+}
+main()
+{
+    int valid_flag = 0;
+    char password[1024];
+    while (1)
+    {
+        printf("please input password: ");
+        int valid_flag = 0;
+        char password[1024];
+        while (1)
+        {
+            printf("please input password: ");
+            scanf("%s", password);
+            valid_flag = verify_password(password);
+            if (valid_flag)
+            {
+                printf("incorrect password!\n\n");
+            }
+            else
+            {
+                printf("Congratulation! You have passed the verification!\n");
+                break;
+            }
+        }
+    }
+}
