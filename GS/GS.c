3648334dee9d401a8db574f10138674f21889d9f
yukar1z0e <44317939+yukar1z0e@users.noreply.github.com>
2019-10-12 17:11:05 +0800

    GS

---
+++ GS/GS.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include"string.h"
+
+int vulfuction(char* str)
+{
+	char arry[4];
+	strcpy(arry, str);
+	return 1;
+}
+int main(int argc, char* argv[])
+{
+	char* str = "yeah,the fuction is without GS";
+	vulfuction(str);
+	return 0;
+}
