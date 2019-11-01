06a52d8cbf8013555449947f3690ec5a94863881
yukar1z0e <44317939+yukar1z0e@users.noreply.github.com>
2019-11-01 16:03:23 +0800

    ASCII and Unicode ROR13 hash functions

--- Shellcode1/Shellcode1.cpp
+++ Shellcode1/Shellcode1.cpp
@@ -4,11 +4,37 @@
 // 禁用堆栈缓冲区检查（ /Gs标志）。在函数的开头和结尾处调用的堆栈cookie检查器函数位于二进制文件中与位置相关的静态位置，因此，输出的任何函数都无法重定位且对于shellcode无效。
 
 #include <stdio.h>
+#include <Windows.h>
 
 void shell_code()
 {
 	for (;;)
 		;
+}
+
+DWORD __stdcall unicode_ror13_hash(const WCHAR* unicode_string)
+{
+	DWORD hash = 0;
+
+	while (*unicode_string != 0)
+	{
+		DWORD val = (DWORD)*unicode_string++;
+		hash = (hash >> 13) | (hash << 19); // ROR 13
+		hash += val;
+	}
+	return hash;
+}
+
+DWORD __stdcall ror13_hash(const char* string)
+{
+	DWORD hash = 0;
+
+	while (*string) {
+		DWORD val = (DWORD)*string++;
+		hash = (hash >> 13) | (hash << 19);  // ROR 13
+		hash += val;
+	}
+	return hash;
 }
 
 void __declspec(naked) END_SHELLCODE(void) {}
