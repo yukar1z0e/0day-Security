5d77bbc413882b85a47da2cc6b6349753fc2978c
yukar1z0e <44317939+yukar1z0e@users.noreply.github.com>
2019-11-06 15:18:14 +0800

    反汇编

--- scanf/scanf.cpp
+++ scanf/scanf.cpp
@@ -15,13 +15,42 @@ int main()
 	return 0;
 };
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
 
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
+	//1: // scanf.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+	//2 : //
+	//3 :
+	//4 : #include <stdio.h>
+	//5 :
+	//6 : int main()
+	//7 : {
+	//	005E1100 55                   push        ebp
+	//	005E1101 8B EC                mov         ebp, esp
+	//	005E1103 51                   push        ecx
+	//8: 	int x;
+	//9: 	printf("Enter X:\n");
+	//	005E1104 68 08 21 5E 00       push        5E2108h
+	//	005E1109 E8 42 FF FF FF       call        printf(05E1050h)
+	//	005E110E 83 C4 04             add         esp, 4
+	//10:
+	//11 : scanf("%d", &x);
+	//	005E1111 8D 45 FC             lea         eax, [x]
+	//	005E1114 50                   push        eax
+	//10:
+	//11 : scanf("%d", &x);
+	//	005E1115 68 14 21 5E 00       push        5E2114h
+	//	005E111A E8 A1 FF FF FF       call        scanf(05E10C0h)
+	//	005E111F 83 C4 08             add         esp, 8
+	//	12:
+	//13 : printf("You entered %d...\n", x);
+	//	005E1122 8B 4D FC             mov         ecx, dword ptr[x]
+	//	005E1125 51                   push        ecx
+	//	005E1126 68 18 21 5E 00       push        5E2118h
+	//	005E112B E8 20 FF FF FF       call        printf(05E1050h)
+	//	005E1130 83 C4 08             add         esp, 8
+	//	14:
+	//15 : return 0;
+	//	005E1133 33 C0 xor eax, eax
+	//16: };
+	//	005E1135 8B E5                mov         esp, ebp
+	//	005E1137 5D                   pop         ebp
+	//	005E1138 C3                   ret
