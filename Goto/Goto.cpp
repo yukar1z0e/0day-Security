391a740ff1e413c44fd611f7d3d90f5daf680aee
yukar1z0e <44317939+yukar1z0e@users.noreply.github.com>
2019-11-07 17:16:25 +0800

    disassembly

--- Goto/Goto.cpp
+++ Goto/Goto.cpp
@@ -12,13 +12,36 @@ exit:
 	printf("end\n");
 };
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
+/*
+	 1: // Goto.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+	 2: //
+	 3:
+	 4: #include <stdio.h>
+	 5:
+	 6: int main()
+	 7: {
+00941080 55                   push        ebp
+00941081 8B EC                mov         ebp,esp
+	 8: 	printf("begin\n");
+00941083 68 08 21 94 00       push        942108h
+00941088 E8 B3 FF FF FF       call        printf (0941040h)
+0094108D 83 C4 04             add         esp,4
+	 9: 	goto exit;
+00941090 EB 0F                jmp         exit (09410A1h)
+00941092 EB 0D                jmp         exit (09410A1h)
+	10: 	printf("skip me!\n");
+00941094 68 10 21 94 00       push        942110h
+00941099 E8 A2 FF FF FF       call        printf (0941040h)
+0094109E 83 C4 04             add         esp,4
+	11: exit:
+	12: 	printf("end\n");
+009410A1 68 1C 21 94 00       push        94211Ch
+009410A6 E8 95 FF FF FF       call        printf (0941040h)
+	11: exit:
+	12: 	printf("end\n");
+009410AB 83 C4 04             add         esp,4
+	13: };
+009410AE 33 C0                xor         eax,eax
+009410B0 5D                   pop         ebp
+009410B1 C3                   ret
+*/
