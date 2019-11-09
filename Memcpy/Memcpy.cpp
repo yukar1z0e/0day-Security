458b8a74be814c80df44a6f859c129d4b3d68201
yukar1z0e <44317939+yukar1z0e@users.noreply.github.com>
2019-11-09 14:52:42 +0800

    memcpy

---
+++ Memcpy/Memcpy.cpp
@@ -0,0 +1,15 @@
+﻿// Memcpy.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <stdio.h>
+
+void my_memcpy(unsigned char* dst, unsigned char* src, size_t cnt)
+{
+	size_t i;
+	for (i = 0; i < cnt; i++)
+		dst[i] = src[i];
+};
+
+int main() {
+	return 0;
+}
