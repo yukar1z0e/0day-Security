fdd0e3b1b0c7bd1512245636a8a52ce7f19d6b54
yukar1z0e <44317939+yukar1z0e@users.noreply.github.com>
2019-11-11 10:33:37 +0800

    线性同余法

---
+++ Random/Random.cpp
@@ -0,0 +1,21 @@
+﻿// Random.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <stdint.h>
+
+#define RNG_a 1664525
+#define RNG_c 1013904223
+
+static uint32_t rand_state;
+
+void my_srand(uint32_t init)
+{
+	rand_state = init
+}
+
+int my_rand()
+{
+	rand_state = rand_state * RNG_a;
+	rand_state = rand_state + RNG_c;
+	return rand_state & 0x7fff;
+}
